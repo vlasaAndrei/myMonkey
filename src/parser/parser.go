eecc63a3abbb79933b2c2b9c6bc12973ee22d827
vlasaAndrei <vlasa.andrei@agilefreaks.com>
2026-08-02 16:42:56 +0300

    scaffold parser init funcs

---
+++ src/parser/parser.go
@@ -0,0 +1,34 @@
+package parser
+
+import (
+	"myMonkey/src/ast"
+	"myMonkey/src/lexer"
+	"myMonkey/src/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// Read two tokens, so curToken and peekToken are both set
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
